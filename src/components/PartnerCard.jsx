773e86180471a081e73c71db8a1ce3b194f95a53
6upe <katongobupe444@gmail.com>
2026-04-12 19:40:49 +0200

    Update: Partners Data

--- src/components/PartnerCard.jsx
+++ src/components/PartnerCard.jsx
@@ -7,26 +7,26 @@ const PartnerCard = ({ name, logo, description, alt}) => {
 
   return (
     <div className="col-lg-6 col-md-6 col-12">
-      {/* Single Team Member */}
-      <div className="single-news team-card">
-        <div className="news-head team-card__image-container">
+      {/* Single partner Member */}
+      <div className="single-news partner-card">
+        <div className="news-head partner-card__image-container">
           <img
             src={logo}
             alt='no logo'
-            className="team-card__image"
+            className="partner-card__image"
           />
 
         </div>
         <div className="news-body">
           <div className="news-content mx-2">
-            <h2 className="team-card__name">{name}</h2>
-            {description && <p className="text team-card__role">{description}</p>}
+            <h2 className="partner-card__name">{name}</h2>
+            {description && <p className="text partner-card__role">{description}</p>}
           </div>
         </div>
 
-        <span className="team-card__underline" aria-hidden="true"></span>
+        <span className="partner-card__underline" aria-hidden="true"></span>
       </div>
-      {/* End Single Team Member */}
+      {/* End Single partner Member */}
     </div>
   );
 };
