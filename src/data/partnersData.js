f1f2dee1957e3b53ce8e3ca65f31e43034c16113
Matthew <matt2110m@gmail.com>
2026-05-04 11:52:30 +0100

    Changes to be committed:  Partner pages w/ logo update

--- src/data/partnersData.js
+++ src/data/partnersData.js
@@ -1,65 +1,73 @@
 export const  partnersData = [
     {
         id: 1,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/3.png'
+        name: 'Tevel',
+        description: 'Partnered with Africa Access Water (AfAW) on the Mphande Hills Development Project, working across three villages in Kafue District, Lusaka Province. Providing solar powered water facilities on communal land using the AfAW model and has four sites with three extra clean drinking water kiosks.',
+        logo: '/img/partners/Tevel_Logo.png'
     },
 
     {
         id: 2,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/4.png'
+        name: 'Segal Family Foundation',
+        description: 'Provides institutional support through a combination of unrestricted funding and capacity-building initiatives. This includes offering platforms for organizational growth and leadership development, such as supporting the CEO through the African Visionary Fellowship.',
+        logo: '/img/partners/Segal_Logo.png'
     },
 
     {
         id: 3,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/5.png'
+        name: 'Nelly Mutti Foundation',
+        description: 'Partnered with AfAW in establishing the Makeni Solar-Powered Water Demonstration Site. This site showcases AfAW’s model to high-profile stakeholders, including Members of Parliament and government officials, with the aim of promoting replication, government uptake, and sustainable financing mechanisms.',
+        logo: '/img/partners/nmf.png'
     },
     
     {
         id: 4,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/nmf.png'
+        name: 'Baba Group of Companies',
+        description: 'A long-standing partner that has supported AfAW from its inception. Contributions include funding the first project in Shibuyunji, supporting office operations and administration, and ongoing investment in projects through borehole drilling and well construction. The group has also supported the Masopo Project in Choma.',
+        logo: '/img/partners/Baba_Logo.png'
     },
 
     {
         id: 5,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/7.png'
+        name: 'NGO WASH Forum',
+        description: 'The largest network of local and international NGOs and CSOs working in water, sanitation, and hygiene in Zambia. It provides AfAW with capacity-building opportunities, sector visibility, and strategic engagement platforms.',
+        logo: '/img/partners/NGO_WASH_Logo.png'
     },
 
     {
         id: 6,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/8.png'
+        name: 'Google',
+        description: ' Has supported AfAW with approximately USD 10,000 in Google Ads grants and provides access to premium digital tools, including professional email services and other online platforms.',
+        logo: '/img/partners/Google_Logo.png'
     },
 
     {
         id: 7,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/cej.jfif'
+        name: 'Ministry of Water Development and Sanitation',
+        description: 'As the primary government body responsible for water service provision, the Ministry works in alignment with AfAW’s efforts to complement national service delivery. AfAW has partnered with the Ministry on initiatives such as the Water Economy Dialogue, which promotes water for productive use and positions water as a key economic driver.',
+        logo: '/img/partners/Ministry_of_Water_Logo'
     },
 
     {
         id: 8,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/10.png'
+        name: 'Centre for Environmental Justice (CEJ)',
+        description: 'Partnered with AfAW on environmental aspects of project implementation, ensuring sustainability and compliance with environmental standards.',
+        logo: '/img/partners/CEJ_Logo.png'
     },
 
     {
         id: 9,
-        name: 'Partner Organisan Name (PON)',
-        description: 'Description of what the Partner Organisan Name (PON) does, lorem ipsum dore sit ammet ',
-        logo: '/img/partners/stripe.png'
+        name: 'MASHAV (Israel’s Agency for International Development Cooperation)',
+        description: 'Provided support to the Shibuyunji project by expanding water infrastructure and fencing operational areas to enhance sustainability and security.',
+        logo: '/img/partners/MASHAV_Logo.png'
     },
+
+    {
+        id: 10,
+        name: 'Maurice Dillon',
+        description: 'A long-term supporter of Africa Access Water (AfAW), who initially provided a catalytic contribution of USD 20,000. He has continued to support the organization’s work over time and regularly visits project sites, at least twice a year, to provide oversight, encouragement, and strategic engagement.',
+        logo: '/img/partners/MauriceDillon_Logo.png'
+    },
+
 
 ];
