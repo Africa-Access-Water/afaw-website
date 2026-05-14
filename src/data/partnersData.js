9c9057f1e8814f918790624c2d3111748fd217b4
Matthew <matt2110m@gmail.com>
2026-05-14 19:43:06 +0100

    modified:   src/data/partnersData.js

--- src/data/partnersData.js
+++ src/data/partnersData.js
@@ -4,7 +4,7 @@ export const  partnersData = [
         name: 'Tevel',
         description: 'Partnered with Africa Access Water (AfAW) on the Mphande Hills Development Project, working across three villages in Kafue District, Lusaka Province. Providing solar powered water facilities on communal land using the AfAW model and has four sites with three extra clean drinking water kiosks.',
         logo: '/img/partners/Tevel_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://tevelbtzedek.org/tevel_partners/africa-access-water/'
     },
 
     {
@@ -12,7 +12,7 @@ export const  partnersData = [
         name: 'Segal Family Foundation',
         description: 'Provides institutional support through a combination of unrestricted funding and capacity-building initiatives. This includes offering platforms for organizational growth and leadership development, such as supporting the CEO through the African Visionary Fellowship.',
         logo: '/img/partners/Segal_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://www.segalfamilyfoundation.org/partners/africa-access-water/'
     },
 
     {
@@ -20,7 +20,7 @@ export const  partnersData = [
         name: 'Nelly Mutti Foundation',
         description: 'Partnered with AfAW in establishing the Makeni Solar-Powered Water Demonstration Site. This site showcases AfAW’s model to high-profile stakeholders, including Members of Parliament and government officials, with the aim of promoting replication, government uptake, and sustainable financing mechanisms.',
         logo: '/img/partners/nmf.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://www.parliament.gov.zm/node/12251'
     },
     
     {
@@ -28,7 +28,7 @@ export const  partnersData = [
         name: 'Baba Group of Companies',
         description: 'A long-standing partner that has supported AfAW from its inception. Contributions include funding the first project in Shibuyunji, supporting office operations and administration, and ongoing investment in projects through borehole drilling and well construction. The group has also supported the Masopo Project in Choma.',
         logo: '/img/partners/Baba_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'http://www.babadrill.com/'
     },
 
     {
@@ -36,7 +36,7 @@ export const  partnersData = [
         name: 'NGO WASH Forum',
         description: 'The largest network of local and international NGOs and CSOs working in water, sanitation, and hygiene in Zambia. It provides AfAW with capacity-building opportunities, sector visibility, and strategic engagement platforms.',
         logo: '/img/partners/NGO_WASH_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://ngowash.co.zm/'
     },
 
     {
@@ -52,7 +52,7 @@ export const  partnersData = [
         name: 'Google',
         description: ' Has supported AfAW with approximately USD 10,000 in Google Ads grants and provides access to premium digital tools, including professional email services and other online platforms.',
         logo: '/img/partners/Google_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://www.google.com/'
     },
 
     {
@@ -60,7 +60,7 @@ export const  partnersData = [
         name: 'Ministry of Water Development and Sanitation',
         description: 'As the primary government body responsible for water service provision, the Ministry works in alignment with AfAW’s efforts to complement national service delivery. AfAW has partnered with the Ministry on initiatives such as the Water Economy Dialogue, which promotes water for productive use and positions water as a key economic driver.',
         logo: '/img/partners/Ministry_of_Water_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://www.mwds.gov.zm/'
     },
 
     {
@@ -68,7 +68,7 @@ export const  partnersData = [
         name: 'Centre for Environmental Justice (CEJ)',
         description: 'Partnered with AfAW on environmental aspects of project implementation, ensuring sustainability and compliance with environmental standards.',
         logo: '/img/partners/CEJ_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://cejzambia.org/'
     },
 
     {
@@ -76,7 +76,7 @@ export const  partnersData = [
         name: 'MASHAV (Israel’s Agency for International Development Cooperation)',
         description: 'Provided support to the Shibuyunji project by expanding water infrastructure and fencing operational areas to enhance sustainability and security.',
         logo: '/img/partners/MASHAV_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'https://mashav.mfa.gov.il/'
     },
 
     {
@@ -84,7 +84,7 @@ export const  partnersData = [
         name: 'Maurice Dillon',
         description: 'A long-term supporter of Africa Access Water (AfAW), who initially provided a catalytic contribution of USD 20,000. He has continued to support the organization’s work over time and regularly visits project sites, at least twice a year, to provide oversight, encouragement, and strategic engagement.',
         logo: '/img/partners/MauriceDillon_Logo.png',
-        url: 'https://www.silentdonor.com/donate-now-africa-access-water/'
+        url: 'linkedin.com/in/maurice-dillon-869b642b9/'
     },
 
 
