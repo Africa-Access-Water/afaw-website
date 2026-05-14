a251cd98d120130e2cda30a80c8818c521dc9a75
Matthew <matt2110m@gmail.com>
2026-05-14 19:46:29 +0100

    modified:   src/pages/PartnersPage.jsx

--- src/pages/PartnersPage.jsx
+++ src/pages/PartnersPage.jsx
@@ -44,28 +44,25 @@ const PartnersPage = () => {
       {/* Our Board Members */}
       <div className="container-xxl mt-5">
         <div className="container">
-          {/* <div className="text-center mx-auto mb-5" style={{ maxWidth: "600px" }}>
-            <h1 className="mb-3">Our Board Members</h1>
-          </div> */}
-            <div className="row g-4 mx-2 mx-md-0 mx-lg-1">
-                {partners.map((partner, index) => {
-                  return (
-                    <a
-                      key={partner.id || index}
-                      href={partner.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-decoration-none"
-                    >
-                      <PartnerCard 
-                        name={partner.name}
-                        logo={partner.logo || "/img/placeholders/profile.jpg"} 
-                        description={partner.description}
-                      />
-                    </a>
-                  );
-                })}
-            </div>
+          <div className="row g-4 mx-2 mx-md-0 mx-lg-1">
+            {partners.map((partner, index) => {
+              return (
+                <a
+                  key={partner.id || index}
+                  href={partner.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="col-6 text-decoration-none text-dark d-block"
+                >
+                  <PartnerCard 
+                    name={partner.name}
+                    logo={partner.logo || "/img/placeholders/profile.jpg"} 
+                    description={partner.description}
+                  />
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
 
